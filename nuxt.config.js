a54d4c49d091595b3d0ad2233b81c2a6f91f9764
Riswahyudi A <45729526+riswahyudi@users.noreply.github.com>
2021-03-25 09:42:46 +0700

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 let dynamicRoutes = () => {
   const routes = axios
-    .get("https://css-tricks.com/wp-json/wp/v2/posts?page=1&per_page=20")
+    .get("https://www.leskompi.com/wp-json/wp/v2/posts")
     .then(res => {
       return res.data.map(post => `/blog/${post.slug}`)
     })
